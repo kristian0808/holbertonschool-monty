21ee36bdc0bf9427bb647adb2294edc4238dedff
MDrenova <mateodrenova@gmail.com>
2024-01-04 11:51:44 +0000

    Create main file

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments as strings.
+ * Return: 0 upon success, other values for errors.
+ */
+ 
+int main()
+{
+    stack_t *new_node = NULL;
+
+    num = 2;
+    _push(&new_node, 0);
+    _pall(&new_node, 0);
+
+    return (0);
+}
