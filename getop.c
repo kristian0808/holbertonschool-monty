b73b9dd8cc9072b3d62217dbbd57770a512e39a4
kristian0808 <kristianlleshaj@hotmail.com>
2024-01-08 10:37:27 +0100

    Update code

--- getop.c
+++ getop.c
@@ -9,14 +9,6 @@ void (*get_op_code(char *token, __attribute__((unused))unsigned int line))(stack
 		{"pall", _pall},
 		{"swap", _swap},
 		{"add", _add},
-		{"div", _div},
-		{"sub", _sub},
-		{"mul", _mul},
-		{"mod", _mod},
-		{"nop", _nop},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
-		{"rotl", _rotl},
 		{NULL, NULL}
 	};
 	for (i = 0; operation[i].opcode != NULL; i++)
